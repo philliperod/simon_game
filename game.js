7f7656563f719f671c2e5ff97aa8c16ddad11524
Phillipe Rodriguez <philliperod@gmail.com>
2020-09-09 13:22:04 -0500

    added h2 tag for end game repeat; refactor js for checkAnswer function to work further than previously created with for loop and if statements git status end

--- game.js
+++ game.js
@@ -1,35 +1,34 @@
 var buttonColors = ['red', 'blue', 'green', 'yellow'];
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 0;
 
 function nextSequence() {
+  level++;
+  $('#level-title').text('Level ' + level);
   var randomNumber = Math.floor(Math.random() * buttonColors.length);
   var randomChosenColors = buttonColors[randomNumber];
   gamePattern.push(randomChosenColors);
 
-  $('#' + randomChosenColors).on('click', function() {
-    $('#' + randomChosenColors)
-      .animate({opacity: 0.25}, 100)
-      .animate({opacity: 1}, 50);
-  });
-
-  playSound(randomChosenColors);
-  return randomChosenColors;
-}
-
-$('.btn').on('click', function() {
-  var userChosenColors = this.id;
-  userClickedPattern.push(userChosenColors);
-  animatePress(userChosenColors);
-  //playSound(userChosenColors);
-});
-
-function playSound(name) {
-  $('#' + name)
+  $('#' + randomChosenColors)
     .animate({opacity: 0.25}, 100)
     .animate({opacity: 1}, 50);
+  playSound(randomChosenColors);
+
+  // this is a randomize sequence for which the player will have to follow
+  // it will pick a random number from 0 to 1 multiple by the length of buttonColors array
+  // it will store the random number in a variable then use that number to choose a random index value in the buttonColor array
+  // it will then add that index value at the end in gamePattern array
+  // for each id color chosen it will animate a fade-in and fade-out effect
+  // it will attached a click eventListener event to whatever id color is chosen and animate a fadeIn and fadeOut by reducing and increading the opacity
+  // it will play associated sound based on random color chosen and return it
+}
+
+function playSound(name) {
   var soundColor = new Audio('sounds/' + name + '.mp3');
   soundColor.play();
+
+  // this will generate the associated sound from a parameter that matches
 }
 
 function animatePress(currentColor) {
@@ -37,4 +36,51 @@ function animatePress(currentColor) {
   setTimeout(function() {
     $('#' + currentColor).removeClass('pressed');
   }, 100);
+
+  // this will animate a svg drop-shadow highlight when a button is pressed
 }
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    var count = 0;
+
+    for (var i = 0; i < gamePattern.length; i++) {
+      if (gamePattern[i] === userClickedPattern[i]) {
+        count++;
+      }
+    }
+    if (count === gamePattern.length) {
+      console.log('success');
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log('failed');
+    var failedAudio = new Audio('sounds/wrong.mp3');
+    failedAudio.play();
+    $(document).addClass('game-over');
+    setTimeout(function() {
+      $(document).removeClass('game-over');
+    }, 100);
+  }
+}
+
+$('.btn').on('click', function() {
+  var userChosenColors = this.id;
+  userClickedPattern.push(userChosenColors);
+  animatePress(userChosenColors);
+  playSound(userChosenColors);
+
+  // this will select the btn class and attached a click eventListener
+  // it will take in the id of the input and store it into variable userChosenColors
+  // it will add that id name at the end of the userClickedPattern array
+  // it will animate a drop-shadow effect and play the associated sound that matches the id name whenever the button is clicked
+});
+
+$(document).on('keypress', function() {
+  nextSequence();
+  if (level > 0) {
+    $(document).off('keypress');
+  }
+});
